ff6a5af06a4633848b0322777574cbc15ef3da2f
kartik385 <kartikb385@gmail.com>
2021-02-26 10:53:43 +0530

    2/26/2021

--- function.js
+++ function.js
@@ -35,3 +35,41 @@ sayName();
 {let house="Gryffindor";console.log(house)};
 {let house="RavenClaw";console.log(house)};
 {let house=`Slytherin`;console.log(house)};
+
+//Providing parameters(Function with parameters)
+
+function addNumber(a,b) {
+    return a+b;
+    
+}
+addNumber(5,4);
+
+function student(name,house,hp) {
+    return{name,house,hp};
+    
+}
+let stu1=student(`Ron`,`Gryffindor`,50);
+let stu2=student(`Cedric`,`HufflePuff`,100);
+console.log(stu1);
+console.log(stu2);
+
+
+//Argument object
+function user() {
+    return arguments;
+    
+}
+console.log(user("Kartik",18,'manr'));
+
+//Rest parametr
+function colors(...args) {
+    return args;
+    
+}
+console.log(colors(`red`,`green`,`pink`));
+
+function indInfo(name,house,...subjects){
+    return{name,house,subjects};
+}
+let stu3=indInfo(`Draco`,`Slytherin`,`Quiddich`,`Transformation`,`DADA`,`Herbology`);
+console.log(stu3);
