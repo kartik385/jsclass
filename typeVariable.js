4b7ba67d84d48141d910edcdac31dc121676b338
kartik385 <kartikb385@gmail.com>
2021-02-27 13:52:57 +0530

    adding variable

---
+++ typeVariable.js
@@ -0,0 +1,19 @@
+//1primitive type
+let x=3;
+let y=4;
+y=x;
+x=7;
+console.log(y);
+
+//Reference Type
+
+function Car(speed,seats,fuel) {
+    this.speed=speed;
+    this.seats=seats;
+    this.fuel=fuel;
+    
+}
+let tesla=new Car(250,5,`Electric`);
+let porche=tesla;
+tesla.speed=300;
+console.log(porche); 
