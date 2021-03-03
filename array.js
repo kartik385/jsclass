25a4bac1e49926103e651c9e972f60a052a00278
kartik385 <kartikb385@gmail.com>
2021-03-03 10:25:21 +0530

    3/3/21

---
+++ array.js
@@ -0,0 +1,10 @@
+//Array Declaration
+let teams=[`NZ`,`ENG`,`AUS`,`IND`];
+console.log(`Size:${teams.length}`);
+console.log(teams);
+console.log(typeof teams);
+
+//Indexing of Array
+let team1=teams[0];
+let team2=teams[1];
+console.log(team1);
