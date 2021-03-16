2e628b9ad7912b295534550c115ac11d61fd9d4f
kartik385 <kartikb385@gmail.com>
2021-03-16 10:46:21 +0530

    16/3/21

--- object2.js
+++ object2.js
@@ -78,3 +78,91 @@ console.log(singer5Values);
 
 let singer5Array=Object.entries(singer5);
 console.log(singer5Array);
+
+//this keyword in object
+
+let obj6={
+    name:`Tom`,
+    password:3214,
+    userInfo:function(){
+        console.log(this.name);
+    }
+}
+obj6.userInfo();//for normal functions this refers current object;
+
+let obj7={
+    name:`Larry`,
+    password:1234,
+    userInfo:()=>{
+        console.log(this);
+    }
+}
+obj7.userInfo();//for arrow functions this refers window object
+
+//call method
+
+
+let singer6={
+    name:`Ava Max`,
+    song:`Sweet But Psycho`,
+    getInfo:function(){
+        console.log(`My name is ${this.name} & my song is ${this.song}` );
+    }
+}
+
+let singer7={
+    name:`Eminem`,
+    song:`Godzilla`
+}
+
+let singer8={
+    name:`Marshmellow`,
+    song:`Happier`
+}
+singer6.getInfo();
+singer6.getInfo.call(singer7);
+singer6.getInfo.call(singer8);
+
+//2nd way
+
+function getInfoF() {
+    return `I am ${this.name},my song is ${this.song}`;
+    
+}
+console.log(getInfoF.call(singer6));
+console.log(getInfoF.call(singer7));
+console.log(getInfoF.call(singer8));
+
+//apply
+
+function getInfoC(billboardUS,billboardUK) {
+    return `${this.song} composed by ${this.name} placed ${billboardUS} in US and ${billboardUK} in UK`;
+    
+}
+console.log(getInfoC.call(singer6,4,5));
+console.log(getInfoC.apply(singer7,[1,2]));//apply takes arguments as in form of array
+
+function getInfoA(...args){
+    return `${this.song} composed by ${this.name} placed in ${args}`;
+}
+console.log(getInfoA.apply(singer8,[7,8,9,10]));
+
+//bind
+
+let bound=getInfoA.bind(singer6);
+console.log(bound);// bind creates new function
+
+//call example 2
+
+let numbers={num:10};
+function calculate(num2,num3) {
+    return this.num+num2+num3;
+    
+}
+
+console.log(calculate.call(numbers,10,20));
+console.log(calculate.apply(numbers,[10,20]))
+let resultWithBind=calculate.bind(numbers,10,20);
+console.log(resultWithBind);
+console.log(resultWithBind());
+
