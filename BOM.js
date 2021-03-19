a0d0f3433f7a4a7296ae170d53e18763bc9f2636
kartik385 <kartikb385@gmail.com>
2021-03-19 12:50:40 +0530

    19/3/2

--- BOM.js
+++ BOM.js
@@ -1,92 +1,92 @@
 
-// console.log(window.document);
-// console.log(window.navigator);
-// console.log(window.screen);
-// console.log(window.history);
-// console.log(window.location);
+// // console.log(window.document);
+// // console.log(window.navigator);
+// // console.log(window.screen);
+// // console.log(window.history);
+// // console.log(window.location);
 
-//Get lattitude and longitude;
+// //Get lattitude and longitude;
 
-// function getLocation() {
-//     if(window.navigator.geolocation){
-//         window.navigator.geolocation.getCurrentPosition((x)=>{
-//             console.log(x.coords.latitude);
-//             document.write(
-//                 `My current lattitude is <h1>${x.coords.latitude}</h1>
-//                 My current longitude is <h1>${x.coords.longitude}</h1>`
+function getLocation() {
+    if(window.navigator.geolocation){
+        window.navigator.geolocation.getCurrentPosition((x)=>{
+            console.log(x.coords.latitude);
+            document.write(
+                `My current lattitude is <h1>${x.coords.latitude}</h1>
+                My current longitude is <h1>${x.coords.longitude}</h1>`
 
-//             );
-//         });
+            );
+        });
 
-//     }
-//     else{
-//         document.write(`Failed to find your coordinates`);
-//         console.log(`Not supported`);
-//     }
+    }
+    else{
+        document.write(`Failed to find your coordinates`);
+        console.log(`Not supported`);
+    }
     
-//  }
-//  getLocation();
+ }
+ getLocation();
 
-//online or not
+// //online or not
 
-// console.log(window.navigator.online);
+// // console.log(window.navigator.online);
 
-//  if(window.navigator.online===true){
-//      document.write(`Hello`);
+// //  if(window.navigator.online===true){
+// //      document.write(`Hello`);
 
-//  }
-//  else{
-//      document.write(`Hell!`);
-//  }
+// //  }
+// //  else{
+// //      document.write(`Hell!`);
+// //  }
 
-//video
+// //video
 
-// window.navigator.getUserMedia=window.navigator.getUserMedia||window.navigator.webkitGetUserMedia;
-// if(window.navigator.getUserMedia){
-//     window.navigator.getUserMedia(
-//         {audio:true,video:{height:600,width:600}},
-//         stream=>{
-//             let video=document.querySelector("video");
-//             video.srcObject=stream;
-//             video.onloadedmetadata=()=>video.play();
-//         },
-//         err=>{
-//             console.log(`Error :${err.name}`)
-//         });
+// // window.navigator.getUserMedia=window.navigator.getUserMedia||window.navigator.webkitGetUserMedia;
+// // if(window.navigator.getUserMedia){
+// //     window.navigator.getUserMedia(
+// //         {audio:true,video:{height:600,width:600}},
+// //         stream=>{
+// //             let video=document.querySelector("video");
+// //             video.srcObject=stream;
+// //             video.onloadedmetadata=()=>video.play();
+// //         },
+// //         err=>{
+// //             console.log(`Error :${err.name}`)
+// //         });
+// // }
+// // else{
+// //     console.log(`Not supported!`);
+// // }
+
+// console.log(window.navigator.userAgent);
+
+// console.log(window.navigator.appVersion);
+
+// console.log(window.navigator.product);
+
+// console.log(window.navigator.vendor);
+
+// console.log(window.navigator.language);
+
+// console.log(window.navigator.cookieEnabled);
+
+// console.log(window.navigator.javaEnabled);
+
+// console.log(window.navigator.serviceWorker);
+
+// //screen
+
+// console.log(window.screen);
+// console.log(window.screen.width);
+
+// if((window.screen.width<1024)){
+//     console.log(`Low Resolution`)
 // }
 // else{
-//     console.log(`Not supported!`);
+//     console.log(`Normal Resolution`)
 // }
 
-console.log(window.navigator.userAgent);
-
-console.log(window.navigator.appVersion);
-
-console.log(window.navigator.product);
-
-console.log(window.navigator.vendor);
-
-console.log(window.navigator.language);
-
-console.log(window.navigator.cookieEnabled);
-
-console.log(window.navigator.javaEnabled);
-
-console.log(window.navigator.serviceWorker);
-
-//screen
-
-console.log(window.screen);
-console.log(window.screen.width);
-
-if((window.screen.width<1024)){
-    console.log(`Low Resolution`)
-}
-else{
-    console.log(`Normal Resolution`)
-}
-
-document.getElementById("btn").addEventListener("click",e=>{
-    window.location.assign("https://www.spacex.com");
-});
+// document.getElementById("btn").addEventListener("click",e=>{
+//     window.location.assign("https://www.spacex.com");
+// });
 
--- DOM.js
+++ DOM.js
@@ -15,3 +15,57 @@ console.log(document.links)//return all links as array
 for(let link of document.links){
     link.style.textDecoration="none";
 }
+
+//selecting element
+
+
+        //get element by id
+let demo=document.getElementById(`demo`);
+console.log(demo);
+
+
+        //get element by class
+let class1=document.getElementsByClassName(`one`);
+for(let key of class1){
+    console.log(key);                               
+}
+
+        //get element by tag name
+let tag1=document.getElementsByTagName(`article`);
+for(let key of tag1){
+    console.log(key);
+}
+
+        //querySelector
+
+let query1=document.querySelector("#demo");
+console.log(query1);//returns first element
+
+
+let queryAll=document.querySelectorAll(`article`);
+console.log(queryAll);
+queryAll.forEach(x=>console.log(x));//returns all elements with article element
+
+    //perticular nested tag
+let div1=document.querySelector(`#super .sub div`);
+console.log(div1);
+
+//change the content
+
+        //text content(only text)
+div1.textContent="Cant touch this!"
+
+        //innerHtMl(can use tags)
+div1.style.color="red";
+div1.innerHTML="<h1>Cant touch this!</h1>"
+div1.style.backgroundColor="green";
+div1.style.transform=scale(4);
+
+//Adding attributes(setAttribute)
+
+div1.setAttribute("title","How do yu ")
+
+//adding classes
+
+div1.className="moris";      
+
