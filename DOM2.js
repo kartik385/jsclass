d6bc27892f72d6be3f95d82e1fcf5f1bc39d6ffe
kartik385 <kartikb385@gmail.com>
2021-03-24 10:35:51 +0530

    24/3/21

--- DOM2.js
+++ DOM2.js
@@ -1,1 +1,18 @@
-//Events
+//Eventszzz
+        //Event listeners
+// let button1=document.querySelector(`.btn`);
+// button1.addEventListener(`click`,()=>{
+//     console.log(`clicked`);
+//     document.body.classList.toggle(`active`);
+// })
+let list=document.querySelectorAll(`ul li`);
+list.forEach(x=>{
+    x.addEventListener(`click`,(e)=>{
+        e.preventDefault();
+        console.log(e.target);
+        let drops=e.target.parentNode.children[1];
+        if(drops){
+            drops.classList.toggle(`show`);
+        }
+    });
+});
